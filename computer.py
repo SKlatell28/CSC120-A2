ad1e074d2934eeffb0c8c774ce47c323d855b2fe
Sophia Klatell <sklatell@smith.edu>
2025-02-10 15:41:28 -0500

    Changes to computer.py building a constructor to make a computer. Changes to oo_resale_shop.py to make functions that buy, sell, and refurbish computers and print the stores inventory. rubric included as well.

--- computer.py
+++ computer.py
@@ -1,10 +1,29 @@
 class Computer:
+    
+    #Attributes:
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
-    # What attributes will it need?
+    # The constructor (makes a computer with all its elements)
+    def __init__(self, description:str, processor_type:str, hard_drive_capacity:int,
+    memory:int, operating_system:str, year_made:int, price:int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price =  price
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
 
-    # What methods will you need?
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,85 @@
+from computer import *
+
+
 class ResaleShop:
+    
+    inventory: list
 
-    # What attributes will it need?
+    #Constructor (makes the inventory/list)
+    def __init__(self):
+        self.inventory = []
 
-    # How will you set up your constructor?
-    # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    #Buys a new computer for the shop and adds it to the inventory
+    def buy(self, C: Computer):
+        
+        print("")
+        print(f"Just bought a {C.description} for ${C.price}!")
+        print("")
+        #####
+        self.inventory.append(C)
+    
+    #Prints out everything currently in the stores inventory
+    def print_inventory(self):
+        pass
+        print("")
+        print("Checking Inventory :)")
+        if self.inventory:
+            for i in self.inventory:
+                print(f"You have a {i.description}, with processor_type: {i.processor_type}")
+        else: 
+            print("You actually do NOT own any computers at all right now:)")
+        print("")
+    
+    #Sells the computers in the inventory
+    def sell(self, C: Computer):
+        if C in self.inventory:
+            print("")
+            print(f"Now selling your {C.description} for ${C.price}!")
+            self.inventory.remove(C)
+            print("")
 
-    # What methods will you need?
+
+    #Refurbishes a computer and updates it's price
+    def refurbish(self, C: Computer):
+        if C in self.inventory:
+            print("")
+            print(f"Refurbishing your {C.description} made in {C.year_made}")
+            if (C.year_made < 2000):
+                print("This computer is really too old to sell, consider donating it instead...")
+                C.price = 0
+            elif (C.year_made < 2012):
+                print("We can upgrade this a little...I guess...")
+                print(f"Upgrading the operating system from {C.operating_system}", end=" ")
+                C.operating_system = "macOS Montery"
+                print(f"to {C.operating_system}")
+                C.price += 250
+            elif (C.year_made < 2018):
+                print("We can upgrade this to a newer model!")
+                print(f"Upgrading the operating system from {C.operating_system}", end=" ")
+                C.operating_system = "macOS Montery"
+                print(f"to {C.operating_system}")
+                C.price += 550
+            else:
+                print("Wow! This model is almost new! Still, we can upgrade this to a newer model!")
+                print(f"Upgrading the operating system from {C.operating_system}", end=" ")
+                C.operating_system = "macOS Montery"
+                print(f"to {C.operating_system}")
+                C.price += 1000
+            print(f"The price of your product is now ${C.price}")
+            print("")
+
+
+def main():
+    d = ResaleShop()
+    name: Computer = Computer ("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5",
+                                 1024, 64, "macOS Big Sur", 2013, 1500)
+    d.buy(name)
+    d.print_inventory()
+    d.refurbish(name)
+    d.sell(name)
+    d.print_inventory()
+
+
+if __name__ == "__main__":
+    main()
+
